6082378f05c40b433450642265514c445f73d00a
jhui0530 <81886875+jhui0530@users.noreply.github.com>
2021-04-09 10:42:30 +0800

    Add files via upload

---
+++ MDK-ARM/Assignment1_Blinky/startup_stm32f446xx.d
@@ -0,0 +1,1 @@
+assignment1_blinky\startup_stm32f446xx.o: startup_stm32f446xx.s
---
+++ MDK-ARM/RTE/_Assignment1_Blinky/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Assignment1_Blinky' 
+ * Target:  'Assignment1_Blinky' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
